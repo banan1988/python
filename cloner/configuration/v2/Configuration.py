f622d3ab767974aaa58c8a7a61c55c3b08f2f829
Lukasz Kucharski <banan@b2d.com.pl>
2016-09-12 22:03:36 +0200

    [ConfigurationManager] Implemented new methods. Fixed from_dict in Dict2Object.

--- cloner/configuration/v2/Configuration.py
+++ cloner/configuration/v2/Configuration.py
@@ -17,39 +17,26 @@ class JsonMapper:
 class Dict2Object:
     @classmethod
     def from_dict(cls, dictionary):
-        return cls(**dictionary)
+        if type(dictionary) is dict:
+            return cls(**dictionary)
+        return dictionary
 
 
 class Configuration(JsonMapper):
-    def __init__(self, clusters: dict, haproxy_monitors: dict):
+    def __init__(self, clusters: dict, replayers: dict, haproxy_monitors: dict):
         self.clusters = clusters
+        self.replayers = replayers
         self.haproxy_monitors = haproxy_monitors
-
-    def __str__(self):
-        return "Configuration {" \
-               "clusters: " + str(self.clusters) + ", " + \
-               "haproxy_monitors: " + str(self.haproxy_monitors) + \
-               "}"
 
 
 class Cluster(Dict2Object):
-    def __init__(self, name, haproxy_monitor=None, haproxy_backend_name=None, hosts={}, always_running=False, updater_enabled=False):
+    def __init__(self, name, hosts: dict, haproxy_monitor, haproxy_backend_name=None, always_running=False, updater_enabled=False):
         self.name = name
+        self.hosts = hosts
         self.haproxy_monitor = haproxy_monitor
         self.haproxy_backend_name = haproxy_backend_name
-        self.hosts = hosts
         self.always_running = always_running
         self.updater_enabled = updater_enabled
-
-    def __str__(self):
-        return "Cluster {" \
-               "name: " + str(self.name) + ", " + \
-               "haproxy_monitor: " + str(self.haproxy_monitor) + ", " + \
-               "haproxy_backend_name: " + str(self.haproxy_backend_name) + ", " + \
-               "hosts: " + str(self.hosts) + ", " + \
-               "always_running: " + str(self.always_running) + ", " + \
-               "updater_enabled: " + str(self.updater_enabled) + \
-               "}"
 
 
 class HaProxyMonitor(Dict2Object):
@@ -59,14 +46,23 @@ class HaProxyMonitor(Dict2Object):
 
 
 class Host(Dict2Object):
-    def __init__(self, host_domain, context_path=None, target_hosts=list(), listen_port=8080, allow_url_paths=list(),
-                 disallow_url_paths=list(), url_rewrite_paths=list(), save_responses=False, traffic_rate='100%', http_timeout='20m'):
+    def __init__(self, host_domain, target_hosts: list, allow_url_paths: list, listen_port=8080, context_path=None,
+                 disallow_url_paths: list = None, url_rewrite_paths: list = None, save_responses=False, traffic_rate='100%', http_timeout='20m'):
         self.host_domain = host_domain
         self.target_hosts = target_hosts
         self.traffic_rate = traffic_rate
         self.listen_port = listen_port
-        self.allow_url_paths = allow_url_paths
-        self.disallow_url_paths = disallow_url_paths
+
+        if allow_url_paths:
+            self.allow_url_paths = allow_url_paths
+        else:
+            self.allow_url_paths = list()
+
+        if disallow_url_paths:
+            self.disallow_url_paths = disallow_url_paths
+        else:
+            self.disallow_url_paths = list()
+
         self.url_rewrite_paths = url_rewrite_paths
         self.save_responses = save_responses
         self.http_timeout = http_timeout
@@ -75,14 +71,14 @@ class Host(Dict2Object):
 
 class ConfigurationReader:
     @staticmethod
-    def read(path):
+    def read(path) -> Configuration:
         with open(path) as configuration_file:
             return Configuration.from_json(configuration_file.read())
 
 
 class ConfigurationWriter:
     @staticmethod
-    def write(path, configuration):
+    def write(path, configuration: Configuration):
         with open(path, "w") as configuration_file:
             configuration_file.write(configuration.to_json())
 
@@ -95,20 +91,43 @@ class ClusterAlreadyExists(Exception):
     pass
 
 
+class HaProxyMonitorAlreadyExists(Exception):
+    pass
+
+
+class ValidationException(Exception):
+    pass
+
+
 class ConfigurationManager:
-    __configuration = Configuration({}, {})
+    __configuration = Configuration({}, {}, {})
 
     def __init__(self, path):
         self.path = path
 
     def read(self):
         self.__configuration = ConfigurationReader.read(self.path)
+        # self.validate()
 
     def write(self):
         ConfigurationWriter.write(self.path, self.__configuration)
 
+    def validate(self):
+        for key_name in self.get_clusters().keys():
+            name = self.get_cluster(key_name).hosts
+            print(name)
+            if key_name != name:
+                raise ValidationException("Key %s is different that name in cluster: %s" % (key_name, name))
+
+        for key_name in self.get_haproxy_monitors().keys():
+            name = self.get_haproxy_monitor(key_name).name
+            if key_name != name:
+                raise ValidationException("Key %s is different that name in haproxy monitor: %s" % (key_name, name))
+
     def add_cluster(self, cluster: Cluster):
-        if cluster.name in self.__configuration.clusters:
+        print("Add cluster %s" % cluster.name)
+
+        if cluster.name in self.get_clusters():
             raise ClusterAlreadyExists(cluster.name)
 
         self.__configuration.clusters[cluster.name] = cluster
@@ -117,39 +136,59 @@ class ConfigurationManager:
         return self.__configuration.clusters
 
     def get_cluster(self, name) -> Cluster:
-        return self.__configuration.clusters[name]
+        return Cluster.from_dict(self.get_clusters().get(name))
+
+    def get_hosts(self, cluster_name):
+        return self.get_cluster(cluster_name).hosts
+
+    def get_host(self, cluster_name, host_domain) -> Host:
+        return self.get_hosts(cluster_name).get(host_domain)
 
     def add_host(self, cluster_name, host: Host):
-        print("Add %s to %s" % (host.host_domain, cluster_name))
+        print("Add host %s to cluster %s" % (host.host_domain, cluster_name))
 
         if self.host_domain_exists(host.host_domain):
             raise HostAlreadyExists(host.host_domain)
 
-        copy = self.get_cluster(cluster_name)
-        copy.hosts[host.host_domain] = host
+        self.get_cluster(cluster_name).hosts[host.host_domain] = host
 
     def remove_host(self, cluster_name, host_domain):
-        cluster = self.get_cluster(cluster_name)
-        cluster.hosts.pop(host_domain)
+        self.get_hosts(cluster_name).pop(host_domain)
 
     def host_domain_exists(self, host_domain):
-        for cluster_name in self.__configuration.clusters.keys():
+        for cluster_name in self.get_clusters().keys():
             if host_domain in self.get_cluster(cluster_name).hosts.keys():
                 return True
         return False
 
-    def get_host(self, cluster_name, host_domain):
-        return self.get_cluster(cluster_name, host_domain)
+    def add_haproxy_monitor(self, haproxy_monitor: HaProxyMonitor):
+        print("Add HaProxyMonitor %s" % haproxy_monitor.name)
+
+        if haproxy_monitor.name in self.get_haproxy_monitors():
+            raise HaProxyMonitorAlreadyExists(haproxy_monitor.name)
+
+        self.__configuration.haproxy_monitors[haproxy_monitor.name] = haproxy_monitor
+
+    def get_haproxy_monitors(self):
+        return self.__configuration.haproxy_monitors
+
+    def get_haproxy_monitor(self, name) -> HaProxyMonitor:
+        return self.get_haproxy_monitors().get(name)
 
 
 def main():
     manager = ConfigurationManager("xxx.json")
-    # manager.read()
-    manager.add_cluster(Cluster("cluster-1"))
-    manager.add_cluster(Cluster("cluster-2"))
-    manager.add_cluster(Cluster("cluster-3"))
-    manager.add_host("cluster-1", Host("host-1.domain.com"))
-    manager.add_host("cluster-1", Host("host-2.domain.com"))
+    manager.read()
+
+    monitor = HaProxyMonitor("localhost-9091", "http://localhost:9091/content/csv")
+    manager.add_haproxy_monitor(monitor)
+
+    manager.add_cluster(Cluster("cluster-4", hosts={"host-1.domain.com": Host("host-1.domain.com", "", "")}, haproxy_monitor=monitor.name))
+    manager.add_cluster(Cluster("cluster-5", hosts={}, haproxy_monitor=monitor.name))
+    manager.add_cluster(Cluster("cluster-6", hosts={}, haproxy_monitor=monitor.name))
+    manager.add_host("cluster-4", Host("host-5.domain.com", "", ""))
+    manager.add_host("cluster-5", Host("host-6.domain.com", "", ""))
+    manager.add_host("cluster-6", Host("host-7.domain.com", "", ""))
     manager.write()
 
 
